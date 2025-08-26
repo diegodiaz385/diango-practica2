7055ecae12909c4ed892a7379e5a03aff5570457
diegodiaz385 <diegoagustin385@gmail.com>
2025-08-26 17:54:43 -0400

    mensaje del cambio

--- Sitio/admin.py
+++ Sitio/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import UserCredential
 
-# Register your models here.
+@admin.register(UserCredential)
+class UserCredentialAdmin(admin.ModelAdmin):
+    list_display = ('email', 'github_connected', 'github_username', 'created_at')
+    list_filter = ('github_connected', 'created_at')
+    search_fields = ('email', 'github_username')
+    readonly_fields = ('created_at',)
+    
+    fieldsets = (
+        ('Información Básica', {
+            'fields': ('email', 'password')
+        }),
+        ('GitHub', {
+            'fields': ('github_connected', 'github_username')
+        }),
+        ('Fechas', {
+            'fields': ('created_at',),
+            'classes': ('collapse',)
+        }),
+    )
